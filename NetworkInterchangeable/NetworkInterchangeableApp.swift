093a6a0efd8d1c62c1f5aa61ea99f70f10f02324
Mark Santoro <mark_santoro@comcast.net>
2024-09-04 19:01:37 -0700

    Initial Commit

---
+++ NetworkInterchangeable/NetworkInterchangeableApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInterchangeableApp.swift
+//  NetworkInterchangeable
+//
+//  Created by Mark Santoro on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInterchangeableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
